2512dea8448bff6d0cb9fea71bcb7d6095c85b1f
Menghuan1918 <menghuan2003@outlook.com>
2024-06-24 19:42:15 +0800

    First version

--- main.js
+++ main.js
@@ -1,49 +1,87 @@
 async function recognize(base64, lang, options) {
     const { config, utils } = options;
     const { tauriFetch } = utils;
-    let { apikey, engine } = config;
+    let { apikey, formula, img_correction } = config;
     base64 = `data:image/png;base64,${base64}`;
 
     if (apikey === undefined || apikey.length === 0) {
         throw "apikey not found";
     }
-    if (engine === undefined || engine.length === 0) {
-        engine = "1";
+    if (formula === undefined || formula.length === 0) {
+        formula = "0";
+    }
+    if (img_correction === undefined || img_correction.length === 0) {
+        img_correction = "1";
     }
 
-    let res = await tauriFetch('https://api.ocr.space/parse/image', {
+    let Base_URL = "https://api.doc2x.noedgeai.com/api";
+
+    // Refresh key first
+    let key = await tauriFetch(`${Base_URL}/refresh`, {
         method: "POST",
         header: {
-            apikey,
-            "content-type": "application/x-www-form-urlencoded"
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${apikey}`
+        },
+    });
+
+    if (key.ok) {
+        key = key.data.token;
+    } else {
+        throw JSON.stringify(key);
+    }
+
+    // Upload image and get uuid
+    let uuid = await tauriFetch(`${Base_URL}/platform/async/img`, {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${key}`,
         },
         body: {
-            type: "Form",
-            payload: {
-                base64Image: base64,
-                OCREngine: engine,
-                language: lang
+            file: base64,
+            data: {
+                "option": formula,
+                "img_correction": img_correction
             }
         }
-    })
+    });
 
-    if (res.ok) {
-        const { result } = res.data;
-        const { ErrorMessage, ParsedResults } = result;
-        if (ErrorMessage) {
-            throw ErrorMessage;
-        }
-        if (ParsedResults) {
-            let target = "";
-            for (let i in ParsedResults) {
-                const { ParsedText } = i;
-                target += ParsedText;
-            }
-            return target;
-        } else {
-            throw JSON.stringify(result);
-        }
+    if (uuid.ok) {
+        uuid = uuid.data.uuid;
     } else {
-        throw JSON.stringify(res);
+        throw JSON.stringify(uuid);
+    }
+    
+    // A loop waiting for the result
+    while (true) {
+        let res = await tauriFetch(`${Base_URL}/platform/async/status?uuid=${uuid}`, {
+            method: "GET",
+            header: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${key}`
+            }
+        });
+
+        if (res.ok) {
+            const { status } = res.data;
+            if (status === "success") {
+                let text = "";
+                for (const data of status.pages) {
+                    try {
+                        text += data.md;
+                    } catch (error) {
+                        continue;
+                    }
+                }
+                return text;
+            } else if (status === "processing" || status === "ready") {
+                await new Promise(resolve => setTimeout(resolve, 1000));
+            } else if (status === "pages limit exceeded") {
+                throw JSON.stringify("pages limit exceeded");
+            } else {
+                throw JSON.stringify(res.data);
+            }
+        }
     }
 }
